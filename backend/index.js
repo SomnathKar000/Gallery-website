b892fe99cdee58f7cb610845b1c701aef57f7bc6
SomnathKar000 <somnathkar201901@gmail.com>
2023-04-22 19:35:17 +0530

    new change

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,30 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const port = process.env.PORT || 5000;
-
 // middleware
 
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(
+    express.static(path.resolve(path.dirname(__dirname), "frontend", "build"))
+  );
+  console.log(path.resolve(path.dirname(__dirname), "frontend", "build"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(path.dirname(__dirname), "frontend", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+}
 
 app.use("/api/v1", authRoutes);
 app.use("/api/v1/images", ImageControlRoutes);
